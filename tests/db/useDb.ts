921bcf7c3205556984b8e8b359440d7e52e07427
Alan North <alanscodelog@gmail.com>
2025-07-13 21:23:32 -0300

    deps: updated deps

--- tests/db/useDb.ts
+++ tests/db/useDb.ts
@@ -14,8 +14,10 @@ const migrations = readMigrationFiles({
 	migrationsFolder: folder,
 	migrationsSchema: schema
 })
+
 import { PGlite } from "@electric-sql/pglite"
 import { drizzle } from "drizzle-orm/pglite"
+
 const client = new PGlite()
 const db = drizzle(client)
 
